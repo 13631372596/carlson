353f4c6035a0ac98d38de627dc76adb47208a876
13631372596 <2136898039@qq.com>
2017-11-23 00:12:38 +0800

    update project

--- carlson-demo/carlson-demo-server/src/main/java/com/carlson/demo/server/controller/DemoController.java
+++ carlson-demo/carlson-demo-server/src/main/java/com/carlson/demo/server/controller/DemoController.java
@@ -2,13 +2,21 @@ package com.carlson.demo.server.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/demo")
 public class DemoController {
 
-    @RequestMapping("/index")
-    public String index(){
-        return "index";
+    @RequestMapping("/login")
+    public String login(){
+        return "sso/login";
+    }
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test(){
+        System.out.println("test");
+        return "test";
     }
 }
